f0f1701707c11a2c1c066d53c439bee1682c618a
kellen <kellen931214@gmail.com>
2026-05-03 23:15:25 +0800

    feat:add Model and migrations

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'description', 'price', 'image_url', 'category_id', 'stock'];
+
+    public function category()
+    {
+        return $this -> belongsTo(Category::class);
+    }
+    
+    public function wishlistsByUsers()
+    {
+        return $this -> belongsToMany(User::class , 'wishlists');
+    }
+
+    public function inCartsOfUsers()
+    {
+        return $this -> belongsToMany(User::class , 'carts')->withPivot('quantity');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    
+    public function reviews()
+    { 
+    return $this -> hasMany(Review::class);
+    }
+
+}
