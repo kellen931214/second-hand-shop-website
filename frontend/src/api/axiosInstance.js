7436c09e1e60d44df98327f50e65b7c4495dce5e
kellen <kellen931214@gmail.com>
2026-05-12 23:04:53 +0800

    a

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_API_BASE}/api`,
+    baseURL: `${import.meta.env.VITE_BACKEND_API_BASE}`,
     withCredentials: true, 
     withXSRFToken: true,
     headers: {
